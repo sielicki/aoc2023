616f2eaea988c275964259eb351c93c4f24da4cf
Nicholas Sielicki <git@opensource.nslick.com>
2023-12-01 21:38:24 -0800

    cleanup day2

--- code/day2.py
+++ code/day2.py
@@ -5,6 +5,8 @@ import pathlib
 import operator
 import itertools
 import collections
+from collections import defaultdict
+import math
 
 THIS_FILE = pathlib.Path(__file__)
 THIS_DAY = pathlib.Path(__file__).stem
@@ -13,43 +15,29 @@ INPUTS_DIR = CODE_DIR / ".." / "inputs"
 THIS_INPUT = INPUTS_DIR / pathlib.Path(THIS_DAY).with_suffix(".txt")
 
 def valid_game(d):
-    for k, vs in d.items():
-        if k == 'red':
-            if not all(v <= 12 for v in vs):
-                return False
-        if k == 'blue':
-            if not all(v <= 14 for v in vs):
-                return False
-        if k == 'green':
-            if not all(v <= 13 for v in vs):
-                return False
-    return True
+    mapping = { "red": 12, "blue": 14, "green": 13, }
+    return all(all(v <= mapping[k] for v in vs) for k, vs in d.items())
 
 def minimal_game(d):
     return {k: max(v) for k, v in d.items()}
 
 def game_power(d):
-    ret = 1
-    for x in d.values():
-        ret *= x
-    return ret
+    return math.prod(d.values())
+
+filter_digits = lambda s: "".join(x for x in s if x.isdigit()).strip()
+filter_alpha = lambda s: "".join(x for x in s if not x.isdigit()).strip()
 
 with THIS_INPUT.open('r') as f:
-    ds = collections.defaultdict(lambda: collections.defaultdict(list))
-    for line in f.readlines():
-        line = line.strip()
+    ds = defaultdict(lambda: defaultdict(list))
+    for line in [x.strip() for x in f.readlines()]:
         game_index, rest = line.split(":")
-        games = rest.split(";")
-        for idx, game in enumerate(games):
-            colors = game.split(",")
+        game_index = int(filter_digits(game_index))
+        for game in rest.split(";"):
+            colors = [x.strip() for x in game.split(",")]
             for color in colors:
-                if "red" in color:
-                    ds[game_index]['red'].append(int("".join(x for x in color if x.isdigit())))
-                elif "green" in color:
-                    ds[game_index]['green'].append(int("".join(x for x in color if x.isdigit())))
-                elif "blue" in color:
-                    ds[game_index]['blue'].append(int("".join(x for x in color if x.isdigit())))
+                count = int(filter_digits(color))
+                color = filter_alpha(color)
+                ds[game_index][color].append(count)
 
-    print(sum(int(k.split(" ")[-1]) for k, v in ds.items() if valid_game(v)))
-
-    print(sum(game_power(minimal_game(v)) for v in ds.values()))
+    print("Part 1: ", sum(k for k, v in ds.items() if valid_game(v)))
+    print("Part 2: ", sum(game_power(minimal_game(v)) for v in ds.values()))
